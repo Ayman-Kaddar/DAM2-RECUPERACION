53e110afbaed97d8cc76642f9965a551ef40f2e5
Ayman-Kaddar <71832197+Ayman-Kaddar@users.noreply.github.com>
2021-09-29 17:12:19 +0200

    Add files via upload

--- M6-Recus/UF2/Act01/src/act1/Act1.java
+++ M6-Recus/UF2/Act01/src/act1/Act1.java
@@ -1,21 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package act1;
 
-/**
- *
- * @author Alumne
- */
-public class Act1 {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-    /**
-     * @param args the command line arguments
-     */
+
+
+public class Act1 {
+    public static final String URL = "jdbc:mysql://localhost:3306/";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "";
+
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    }
+    
+    public static Connection connection() {
+
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            //JOptionPane.showMessageDialog(null, "Conexion exitosa");
+        } catch (Exception e) {
+            System.out.println("Error al conectar con la base de datos");
+        }
+        return connection;
     }
     
 }
